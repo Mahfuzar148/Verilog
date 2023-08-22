78c7d35ae6c265c83025481140af180ea0bb10dd
Mahfuzar Rahman <113121842+Mahfuzar148@users.noreply.github.com>
2023-08-22 22:35:50 +0600

    Update Master Slave JK Flip-flop.v

--- Master Slave JK Flip-flop.v
+++ Master Slave JK Flip-flop.v
@@ -1,26 +1,29 @@
 
-module master_jk_flip_flop(q, q_bar, j, k, clk, clr);
+
+module master_jk(q, q_bar, j, k, clk, clr);
   output q, q_bar;
   input j, k, clk, clr;
-  wire a, b, c, d, y, y_bar, clk_bar;
+  wire a, b, c, d, e,f, clk_bar;
 
-  not(y_bar, y);
+  
+ 
+  nand(a, j, clk, clr, q_bar);
+  nand(b, k, clk, q);
+  nand(c, a, d);
+  nand(d, b, c, clr);
+
   not(clk_bar, clk);
 
-  nand(a, j, clk, clr, q_bar);
-  nand(b, k, clk, clr, q);
-  nand(y, a, y_bar);
-  nand(y_bar, b, y, clr);
-  nand(c, y, clk_bar);
-  nand(d, y_bar, clk_bar);
-  nand(q, c, q_bar, clr);
-  nand(q_bar, d, q, clr);
+  nand(e, c, clk_bar);
+  nand(f, d, clk_bar);
+  nand(q, e, q_bar);
+  nand(q_bar, f, q, clr);
 endmodule
 
 module master_jk_test;
   reg j, k, clk, clr;
   wire q, q_bar;
-  master_jk_flip_flop mjk(.q(q), .q_bar(q_bar), .j(j), .k(k), .clk(clk), .clr(clr));
+  master_jk mjk(.q(q), .q_bar(q_bar), .j(j), .k(k), .clk(clk), .clr(clr));
 
   initial begin
     j = 1'b0;
